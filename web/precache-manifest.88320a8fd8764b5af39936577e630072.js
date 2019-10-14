3e34f5e9a3777791960b48d67a07067918aedf3e
Naveen Kumar D <naveenkumar_d@photonint.com>
2019-10-14 17:50:10 +0530

    initial commit.

---
+++ web/precache-manifest.88320a8fd8764b5af39936577e630072.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0added475605b83c0aa5aa7e50a50388",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c55489100a96a5510bf3",
+    "url": "/static/css/2.d7627683.chunk.css"
+  },
+  {
+    "revision": "2ee6ca661dd379beb692",
+    "url": "/static/css/main.9f16f16b.chunk.css"
+  },
+  {
+    "revision": "c55489100a96a5510bf3",
+    "url": "/static/js/2.4883dd4f.chunk.js"
+  },
+  {
+    "revision": "2ee6ca661dd379beb692",
+    "url": "/static/js/main.2dd85c89.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
