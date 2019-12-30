b91d3c48ebb90dda19d919fc754a35d32773cc10
nvnkmrd <56342655+nvnkmrd@users.noreply.github.com>
2019-12-30 18:28:23 +0530

    Deployment for enter button.

--- web/precache-manifest.596df955b4b71f822be4a437e41f2605.js
+++ web/precache-manifest.596df955b4b71f822be4a437e41f2605.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5d256e5da4ba5275af6e5c6f349d14c3",
+    "revision": "b752de120b7531c7f66adc0185b26697",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.f42b7bbd.chunk.css"
   },
   {
-    "revision": "cd5614d224b6900e1332",
+    "revision": "e034ab91dde80834cbda",
     "url": "/static/css/main.70f2f45a.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.5841a4bb.chunk.js"
   },
   {
-    "revision": "cd5614d224b6900e1332",
-    "url": "/static/js/main.66746b1e.chunk.js"
+    "revision": "e034ab91dde80834cbda",
+    "url": "/static/js/main.a8fc767b.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
