1e30c38c77180e60c00d5401977808545ae8d606
nvnkmrd <56342655+nvnkmrd@users.noreply.github.com>
2020-01-02 17:27:52 +0530

    Deployment for web push notification.

---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,37 @@
+importScripts("https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/7.6.1/firebase-messaging.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCrGumtbn2McvNXzZdhShhLaW0QsIFZxOk",
+  authDomain: "nest-df0a7.firebaseapp.com",
+  databaseURL: "https://nest-df0a7.firebaseio.com",
+  projectId: "nest-df0a7",
+  storageBucket: "nest-df0a7.appspot.com",
+  messagingSenderId: "422026408390",
+  appId: "1:422026408390:web:cf0e6a945e7f1ec0d602c7"
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  const promiseChain = clients
+    .matchAll({
+      type: "window",
+      includeUncontrolled: true
+    })
+    .then(windowClients => {
+      for (let i = 0; i < windowClients.length; i++) {
+        const windowClient = windowClients[i];
+        windowClient.postMessage(payload);
+      }
+    })
+    .then(() => {
+      return registration.showNotification("my notification title");
+    });
+  return promiseChain;
+});
+
+self.addEventListener('notificationclick', function(event) {
+  // do what you want
+  // ...
+});
